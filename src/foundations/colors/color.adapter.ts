3d53a02f8f4242e67490b2c621d812886e0f11a4
Camomilte <camomilte@gmail.com>
2026-04-27 13:19:28 +0200

    tokens to json and installed style-dictionary

--- src/foundations/colors/color.adapter.ts
+++
@@ -1,7 +0,0 @@
-import { buildColorItems } from "../../utils/colorItemsBuilder.utils";
-import { color } from "./theme";
-
-export const backgroundColors = buildColorItems(color.background);
-export const interactiveElemenetColors = buildColorItems(color.interative);
-export const textColors = buildColorItems(color.text);
-export const strokeColors = buildColorItems(color.stroke);
