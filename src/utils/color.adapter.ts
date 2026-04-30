415124f133a933c093d49bb8f1b2127c1e5d2432
Camomilte <camomilte@gmail.com>
2026-04-30 17:08:17 +0200

    prettier config, formatted project

--- src/utils/color.adapter.ts
+++ src/utils/color.adapter.ts
@@ -1,4 +1,6 @@
-export const normalizeScale = (scale: Record<string, any>) => // eslint-disable-line @typescript-eslint/no-explicit-any
+export const normalizeScale = (
+  scale: Record<string, any>, // eslint-disable-line @typescript-eslint/no-explicit-any
+) =>
   Object.fromEntries(
-    Object.entries(scale).map(([key, token]) => [key, token.value])
-);
+    Object.entries(scale).map(([key, token]) => [key, token.value]),
+  );
