919d2af0add153a4dca1999074d098e994eff428
Camomilte <camomilte@gmail.com>
2026-04-30 15:10:57 +0200

    lint errors resolved

--- src/utils/color.adapter.ts
+++ src/utils/color.adapter.ts
@@ -1,4 +1,4 @@
-export const normalizeScale = (scale: Record<string, any>) =>
+export const normalizeScale = (scale: Record<string, any>) => // eslint-disable-line @typescript-eslint/no-explicit-any
   Object.fromEntries(
     Object.entries(scale).map(([key, token]) => [key, token.value])
 );
